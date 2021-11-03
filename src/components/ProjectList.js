d54bead1bada2d10841ff9dbdd2430b5b2473bc4
Holly Teunis <+>
2021-11-03 11:50:46 -0400

    DONE

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,14 +1,18 @@
-import React from "react";
 import ProjectItem from "./ProjectItem";
+export default ProjectList;
 
-function ProjectList({ projects }) {
-  console.log(projects);
+function ProjectList(props) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
-    </div>
-  );
-}
-
-export default ProjectList;
+      <div id="project-list">
+      <ul>
+      {props.projects.map(project =>
+        <ProjectItem name={project.name} about={project.about}technologies={project.technologies} id={project.id} key={project.id}/>)}
+  
+      </ul>
+      
+      </div>
+      </div>
+      );
+  }
